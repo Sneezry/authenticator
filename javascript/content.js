d18145dc2385a938fa8198c5aef044c10c6d01d4
rebornix <penn.lv@gmail.com>
2015-04-29 12:08:19 +0800

    Code formatting to meet JSHint requirements

--- javascript/content.js
+++ javascript/content.js
@@ -1,12 +1,12 @@
-function showGrayLayout(){
+function showGrayLayout() {
 	var grayLayout = document.getElementById('__ga_grayLayout__');
-	if(!grayLayout){
+	if (!grayLayout) {
 		grayLayout = document.createElement('div');
 		grayLayout.id = '__ga_grayLayout__';
 		document.body.appendChild(grayLayout);
 		var scan = document.createElement('div');
 		scan.className = 'scan';
-		scan.style.background = 'url('+chrome.extension.getURL('images/scan.gif')+') no-repeat center';
+		scan.style.background = 'url(' + chrome.extension.getURL('images/scan.gif') + ') no-repeat center';
 		grayLayout.appendChild(scan);
 		var captureBox = document.createElement('div');
 		captureBox.id = '__ga_captureBox__';
@@ -14,18 +14,18 @@ function showGrayLayout(){
 		grayLayout.onmousedown = grayLayoutDown.bind(this);
 		grayLayout.onmousemove = grayLayoutMove.bind(this);
 		grayLayout.onmouseup = grayLayoutUp.bind(this);
-		grayLayout.oncontextmenu = function(){
+		grayLayout.oncontextmenu = function () {
 			var e = event || window.event;
 			e.preventDefault();
-		    return false;
-		}
+			return false;
+		};
 	}
 	grayLayout.style.display = 'block';
 }
 
-function grayLayoutDown(event){
+function grayLayoutDown(event) {
 	var e = event || window.event;
-	if(e.button==1 || e.button==2){
+	if (e.button === 1 || e.button === 2) {
 		e.preventDefault();
 		return false;
 	}
@@ -39,78 +39,77 @@ function grayLayoutDown(event){
 	captureBox.style.display = 'block';
 }
 
-function grayLayoutMove(event){
+function grayLayoutMove(event) {
 	var e = event || window.event;
-	if(e.button==1 || e.button==2){
+	if (e.button === 1 || e.button === 2) {
 		e.preventDefault();
 		return false;
 	}
 	var captureBox = document.getElementById('__ga_captureBox__');
 	var captureBoxLeft = Math.min(this.captureBoxLeft, e.clientX);
 	var captureBoxTop = Math.min(this.captureBoxTop, e.clientY);
-	var captureBoxWidth = Math.abs(this.captureBoxLeft - e.clientX)-1;
-	var captureBoxHeight = Math.abs(this.captureBoxTop - e.clientY)-1;
+	var captureBoxWidth = Math.abs(this.captureBoxLeft - e.clientX) - 1;
+	var captureBoxHeight = Math.abs(this.captureBoxTop - e.clientY) - 1;
 	captureBox.style.left = captureBoxLeft + 'px';
 	captureBox.style.top = captureBoxTop + 'px';
 	captureBox.style.width = captureBoxWidth + 'px';
 	captureBox.style.height = captureBoxHeight + 'px';
 }
 
-function grayLayoutUp(event){
+function grayLayoutUp(event) {
 	var e = event || window.event;
 	var grayLayout = document.getElementById('__ga_grayLayout__');
 	var captureBox = document.getElementById('__ga_captureBox__');
-	var captureBoxLeft = Math.min(this.captureBoxLeft, e.clientX)+1;
-	var captureBoxTop = Math.min(this.captureBoxTop, e.clientY)+1;
-	var captureBoxWidth = Math.abs(this.captureBoxLeft - e.clientX)-1;
-	var captureBoxHeight = Math.abs(this.captureBoxTop - e.clientY)-1;
-	setTimeout(function(){
+	var captureBoxLeft = Math.min(this.captureBoxLeft, e.clientX) + 1;
+	var captureBoxTop = Math.min(this.captureBoxTop, e.clientY) + 1;
+	var captureBoxWidth = Math.abs(this.captureBoxLeft - e.clientX) - 1;
+	var captureBoxHeight = Math.abs(this.captureBoxTop - e.clientY) - 1;
+	setTimeout(function () {
 		captureBox.style.display = 'none';
 		grayLayout.style.display = 'none';
 	}, 100);
-	if(e.button==1 || e.button==2){
+	if (e.button === 1 || e.button === 2) {
 		e.preventDefault();
 		return false;
 	}
 	//make sure captureBox and grayLayout is hidden
-	setTimeout(function(){
+	setTimeout(function () {
 		sendPosition(captureBoxLeft, captureBoxTop, captureBoxWidth, captureBoxHeight);
 	}, 200);
 	return false;
 }
 
-function sendPosition(left, top, width, height){
-	chrome.runtime.sendMessage({action: 'position', info: {
-		left: left,
-		top: top,
-		width: width,
-		height: height,
-		windowWidth: window.innerWidth
-	}});
+function sendPosition(left, top, width, height) {
+	chrome.runtime.sendMessage({
+		action : 'position',
+		info : {
+			left : left,
+			top : top,
+			width : width,
+			height : height,
+			windowWidth : window.innerWidth
+		}
+	});
 }
 
-function showQrCode(msg){
-	var left = (screen.width/2)-200;
-	var top = (screen.height/2)-100;
+function showQrCode(msg) {
+	var left = (screen.width / 2) - 200;
+	var top = (screen.height / 2) - 100;
 	var url = chrome.extension.getURL('qr.html') + '?' + encodeURIComponent(msg);
-	window.open(url, '_blank', 'toolbar=no, location=no, status=no, menubar=no, scrollbars=yes, copyhistory=no, width=400, height=200, left='+left+',top='+top);
+	window.open(url, '_blank', 'toolbar=no, location=no, status=no, menubar=no, scrollbars=yes, copyhistory=no, width=400, height=200, left=' + left + ',top=' + top);
 }
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-	if(message.action == 'capture'){
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+	if (message.action === 'capture') {
 		sendResponse('beginCapture');
 		showGrayLayout();
-	}
-	else if(message.action == 'errorsecret'){
-		alert(chrome.i18n.getMessage('errorsecret')+message.secret);
-	}
-	else if(message.action == 'errorqr'){
+	} else if (message.action === 'errorsecret') {
+		alert(chrome.i18n.getMessage('errorsecret') + message.secret);
+	} else if (message.action === 'errorqr') {
 		alert(chrome.i18n.getMessage('errorqr'));
-	}
-	else if(message.action == 'added'){
+	} else if (message.action === 'added') {
 		alert(message.account + chrome.i18n.getMessage('added'));
-	}
-	else if(message.action == 'text'){
+	} else if (message.action === 'text') {
 		showQrCode(message.text);
 	}
 });
